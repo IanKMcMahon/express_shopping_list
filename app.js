43b4baf01c3a42c105bba88d4553c436da317c82
Your Name <you@example.com>
2024-01-13 16:09:43 -0800

    router header

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const morgan = require("morgan")
 const routes = require("routes/routes")
 
 const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server is listening on port 3000");
+  });
+  
