db616666c9129e70502dad0310755894aaa03f26
Your Name <you@example.com>
2024-01-13 21:09:27 -0800

    Finalize tests

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const express = require("express");
-const middleware = require("./middleware")
-const items = require('./fakeDb');
-const morgan = require("morgan")
-
 const routes = require("./routes")
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
-
 app.use('/items', routes)
 
 //////////////////////////////////////////
@@ -19,3 +13,4 @@ app.listen(3000, function () {
     console.log("Server is listening on port 3000");
   });
   
+  module.exports = app;
--- fakeDb.js
+++ fakeDb.js
@@ -1,4 +1,3 @@
-global.items = 
-[{'name': 'popsicle', 'price': 1.45}, {'name':'cheerios', 'price': 3.40}, {'name':'milk', 'price':2.50}]
+global.items = [];
 
 module.exports = items
